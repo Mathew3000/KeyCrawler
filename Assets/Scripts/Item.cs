87c3c5d4c7163afa013cf493216468877c771a9f
anke weber <anke.weber@centigrade.de>
2020-02-01 12:07:08 +0100

    feat:
    add some scripts

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KeyCrawler
+{
+    enum itemKind
+    {
+        key,
+        health,
+        upgrade
+    }
+
+    public interface IItem<Item>
+    {
+         string Kind { get; set; }
+    }
+
+    public class Item : MonoBehaviour, IItem<Item>
+    {
+        private itemKind kind;
+
+        internal itemKind Kind { get => kind; set => kind = value; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
