8acf594d771666aba3d7ad6c31e07583e8dd3233
Matthias Naumann <mathew96@endofnet.org>
2020-02-01 11:23:08 +0100

    Added Player baseclass

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KeyCrawler
+{
+    public interface IPlayerLife
+    {
+        void AddHP();
+
+        void DoDamage();
+
+        float GetLive();
+    }
+
+    public interface IItem
+    {
+
+    }
+
+
+    public class Player : MonoBehaviour, IPlayerLife
+    {
+        #region EditorSettings
+        
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
