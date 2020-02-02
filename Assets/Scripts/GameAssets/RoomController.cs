0390cf058031a148fe07d8f61fded9d2f6130c9e
Jenonator <account@jenonator.com>
2020-02-02 11:19:15 +0100

    Added more Logic

---
+++ Assets/Scripts/GameAssets/RoomController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KeyCrawler
+{
+    public class RoomController : MonoBehaviour
+    {
+        bool bIsClear;
+        public GameObject goRoom;
+        GameObject goAktivRoom;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            bIsClear = false;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<Camera>())
+            {
+                goAktivRoom = Instantiate(goRoom);
+                
+                foreach (GameObject goSpawner in FindObjectsOfType<GameObject>())
+                {
+                    if (goSpawner.GetComponent<EnemySpawner>())
+                    {
+                        goSpawner.GetComponent<EnemySpawner>().bSpawn = !bIsClear;
+                    }
+                }
+                
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.GetComponent<Camera>())
+            {
+                Destroy(goAktivRoom);
+                bIsClear = true;
+            }
+        }
+
+
+
+    }
+}
---
+++ Assets/Scripts/GameAssets/Trap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KeyCrawler
+{
+    public class Trap : MonoBehaviour
+    {
+        public Sprite spOff;
+        bool bIsOn;
+
+        private void Start()
+        {
+            bIsOn = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<Player>() && bIsOn)
+            {
+                //TODO add Enemy too
+                other.GetComponent<Player>().DoDamage(1f);
+                bIsOn = false;
+                this.GetComponent<SpriteRenderer>().sprite = spOff;
+            }
+            
+        }
+    }
+}
